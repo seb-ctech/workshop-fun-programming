caac3fc5142e2075983a57c2bf6a8f26277a68a5
Seb W <thelayeredmind@gmail.com>
2022-05-20 13:34:30 +0200

    Filled Template with Ideas

--- template/functions.js
+++ template/functions.js
@@ -1,9 +1,71 @@
+import "../p5.js"
 
 //==== PURE FUNCTIONS ============
+// This are some tools that generate or modify data -- or other functions ;)
 
-export function fizz(){
-  console.log("blub blub blub...");
-}
+
+// Higher-Order Functions: TOOLS to create Tools
+
+// Compose: Apply value to functions from the inside out like nesting function calls
+export const compose = (...fns) => x => fns.reduceRight((y, f) => f(y), x);
+
+// Pipe: Apply the result of one function to the input of the next, like a pipe
+export const pipe = (...fns) => x => fns.reduce((y, f) => f(y), x);
+
+// Create a list out of the cumulative call of a next state function on a value
+export const unfold = (f, initState) =>
+  f ( (value, nextState) => [ value, ...unfold (f, nextState) ]
+    , () => []
+    , initState
+    )
+
+export const doIf = predicate => doit => elsedo => value => predicate(value) ? doit(value) : elsedo(value)
+export const doOnEven = doIf(x => x % 2 == 0)
+
+// Creates a list of numbers from start to end
+const numbers = start => end =>
+  unfold
+    ( (next, done, n) =>
+        n > end
+          ? done ()
+          : next ( n // value to add to output
+                 , n + 1  // next state
+                 )
+    , start // initial state
+    )
+
+export const circleSpread = res => r => numbers(0)(res)
+  .map((_, i) => Math.PI * 2 / res * i)
+  .map(angle => createVector(Math.sin(angle) * r, Math.cos(angle) * r))
+
+export const circleSpread30 = circleSpread(30)
+export const circleSpread8 = circleSpread(8)
 
 
 //==== IMPURE FUNCTIONS ============
+// These are your tools which have an effect. Think of them as your pen, eraser and scissors.
+
+export const tracepoints = drawF => points => points.forEach((point, i) => i < points.length - 1 ? drawF(point, points[i+1]) : (point, points[0]))
+
+export const drawline = settings => a => b => {
+  push()
+  settings()
+  line(a.x, a.y, b.x, b.y)
+  pop()
+}
+
+// Can you come up with other composable and flexible drawing tools?
+
+export const arrange = setting => n => draw => {
+  numbers(0)(n).forEach(x => {
+    push()
+    setting(x)
+    draw()
+    pop()
+  })
+}
+
+export const log = x => {
+  console.log(x);
+  return x
+}
--- template/sketch.js
+++ template/sketch.js
@@ -1,8 +1,18 @@
 import "../p5.js"
-import {/** Your functions here **/} from "./functions.js"
+import {tracepoints, drawline, circleSpread30, circleSpread8, pipe,/** Your functions here **/
+arrange} from "./functions.js"
 
 let time = 0.0;
 
+// TODO: come up with a framework sketch, that implements an idea that can be expanded on
+// TODO: create a functional modular architecture to change pieces of the algorithm and its behavior
+// TODO: Some components are just concerned with value production, booleans for branching, numbers as parameters.
+// but some are also concerned with high-order operation like consuming and producing functions. Determine
+// key moments in an algorithm that can be extended with whole other algorithms. 
+// TODO: This architecture should be extendable to the top and to the bottom
+// TODO: Contain one complete state data-structure for a uniform handling of parameters
+// TODO: Allow one impure function that mutates the overall state from one frame to the next
+// TODO: Create high abstractions for general-purpose manipulation of data-structures
 
 // === CREATE YOUR ABSTRACT TOOLS FIRST
 
@@ -13,28 +23,27 @@ let time = 0.0;
 
 window.setup = () => {
   createCanvas(window.innerWidth, window.innerHeight)
+  background(0,0,0)
   // Your Setup code here 
   // (Call all your functions here if you are not doing an animation)
-  background(0,0,0)
+  translate(500, 500);
+  arrange(x => {
+   rotate(Math.PI / 8 * x)
+   translate(x * 50, 50)
+  })
+  (10)
+  (() => tracepoints((a, b) => drawline(() => {
+    stroke(255, b.y * 5, a.x * 5)
+    strokeWeight(a.y / 10)
+  })(a)(b))(pipe(x => x.slice(0, 25), x => x.sort( (a, b) => a.x - b.y))(circleSpread30(100))))
 }
 
 
 window.draw = () => {
-  progressTime()
+  const T = frameCount / frameRate(); // time 0.0
   // Your draw code here
-  // (only use if you want to do an Animation)
-  // Use 'frameCount' or the provided "time" variable as base inputs: NO GLOBAL VARIABLES ALLOWED
 }
 
 window.windowResize = () => {
   resizeCanvas(window.innerWidth, window.innerHeight)
-}
-
-
-// ====== IMPURE EVIL FUNCTIONS
-
-function progressTime(){
-  time += deltaTime
-  time = parseFloat(time.toFixed(2))
-  console.log("Time: ", time)
 }
